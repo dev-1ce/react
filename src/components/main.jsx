4ed62b47bf24527719bb475362b9afbe7ca23f0a
Adarsh <adarsh.kumar02291@gmail.com>
2023-10-20 13:45:27 +0530

    first

---
+++ src/components/main.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+
+export default function Main(){
+    return (
+        <main>
+            <h1 className="main--title">Fun facts about React</h1>
+            <ul className="main--facts">
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>Is maintained by facebook</li>
+                <li>Powers thousand of enterprise apps,including mobile apps</li>
+            </ul>
+        </main>
+    )
+}
---
+++ src/components/navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import logo from "../assets/react-js-icon.svg"
+export default function Nav(){
+    return (
+        <nav>
+            <img src= {logo}  alt=""  className="nav--icon"/>
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            <h4 className="nav--title">React-Course Project 1</h4>
+        </nav>
+    )
+}
